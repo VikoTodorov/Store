5c26ad6dcfa3ee8e25b4219b62fc0d355edef5b7
pgmatev <petargabriel.matev@gmail.com>
2020-01-28 17:04:31 +0200

    Adding database
    	Added db.py file
    		User Table
    		Offer Table

---
+++ db.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+DB_NAME = 'store.db'
+
+conn = sqlite3.connect(DB_NAME)
+
+conn.cursor().execute('''CREATE TABLE IF NOT EXISTS users
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email NVARCHAR(255),
+        password TEXT NOT NULL,
+        name TEXT,
+        adress TEXT,
+        phone INTEGER
+    ) ''')
+
+conn.cursor().execute('''CREATE TABLE IF NOT EXISTS offers
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    ) ''')
+
+conn.commit()
+
+class DB:
+    def __enter__(self):
+        self.conn = sqlite3.connect(DB_NAME)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.conn.commit()
