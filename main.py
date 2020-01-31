8d2583f02c1d46642e4d2a6e6fd680d93ce0d5b5
Viko <vikotodorov02@gmail.com>
2020-01-31 16:55:07 +0200

    add working something

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def new_offer():
 
 ##
 
-
+'''
 @app.route('/offers/<int:id>')
 def show_offer(id):
     offer = Offer.find(id)
@@ -92,24 +92,31 @@ def delete_offer(id):
 #     return ?
 
 ##
-
+'''
 @app.route('/register-check', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
     elif request.method == 'POST':
-        user = User.find(request.form['email'])
-        if not user:
-            values = (None, request.form['email'],
-                      User.hash_password(request.form['password']),
-                      request.form['name'], request.form['adress'],
-                      request.form['phone'])
-            User(*values).create()
-            session['email'] = values[1]
-            return redirect(url_for('logged'))
+        if request.form['email'] != "" and request.form['password'] != "" and \
+        request.form['name'] != "" and request.form['adress'] != "" and \
+        request.form['phone'] != "":
+            user = User.find(request.form['email'])
+            if not user:
+                values = (None, request.form['email'],
+                          User.hash_password(request.form['password']),
+                          request.form['name'],
+                          request.form['adress'],
+                          request.form['phone'])
+                User(*values).create()
+                session['email'] = values[1]
+                return redirect(url_for('logged'))
+            else:
+                return render_template('register.html', error="You can't use \
+                                       that email")
         else:
             return render_template('register.html', error="You can't use \
-                                   that email")
+                                  that email")
 
 
 @app.route('/checklogin', methods=['GET', 'POST'])
@@ -117,18 +124,23 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.find(email)
-        if not user:
-            return render_template("/login.html", error="Invalid email or \
-                                   password")
-        elif user.verify_password(password):
-            session['email'] = email
-            return render_template('/logged.html')
+        if request.form['email'] != "":
+            email = request.form['email']
+            password = request.form['password']
+            user = User.find(email)
+            if not user:
+                return render_template("/login.html", error="Invalid email or \
+                                       password")
+            elif user.verify_password(password):
+                session['email'] = email
+                return render_template('/logged.html')
+            else:
+                return render_template("/login.html", error="Invalid email or \
+                                       password")
+
         else:
             return render_template("/login.html", error="Invalid email or \
-                                   password")
+                                       password")
 
 
 @app.route('/logout')
